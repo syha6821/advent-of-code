49a9a925885bc6ef86762bc4ccc3c7846a535430
syha6821 <83122197+syha6821@users.noreply.github.com>
2024-10-01 18:13:45 +0900

    Delete 2015 directory

--- 2015/1/readfile.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include "std_lib_facilities.h"
-#include "pattern.h"
-
-string FileName(int argc, char** argv);
-vector<Pattern> ReadPattern(string filename);
--- 2015/4/test.rkt
+++
@@ -1,1 +0,0 @@
-(test gooddefine "good")
